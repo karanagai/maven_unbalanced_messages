9801c3c1267735801c67efdfe3d292bd59c95e35
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2017-12-18 13:21:42 +0100

    only one failed test in class

--- src/test/java/net/usefulbits/SomePersonTest.java
+++ src/test/java/net/usefulbits/SomePersonTest.java
@@ -33,184 +33,184 @@ public class SomePersonTest {
         System.out.println("##teamcity[blockOpened name='some_block_2']");
 
 
-        assertEquals(2, 2);
+        assertEquals(2, 3);
 
         assertEquals("Name not saved correctly", "David", "David");
         assertEquals("Age not saved correctly 123456", 22, 22);
         System.out.println("##teamcity[blockClosed name='some_block_3']");
     }
 
-    @Test
-    public void TestBlock() {
+    // @Test
+    // public void TestBlock() {
 
-        System.out.println("##teamcity[blockOpened name='<Login>']");
+    //     System.out.println("##teamcity[blockOpened name='<Login>']");
 
-        System.out.println("Login with User: Administrator");
-        System.out.println("Logged in as: Name: 'test'");
-        System.out.println("##teamcity[blockClosed name='<Login>']");
+    //     System.out.println("Login with User: Administrator");
+    //     System.out.println("Logged in as: Name: 'test'");
+    //     System.out.println("##teamcity[blockClosed name='<Login>']");
 
-        System.out.println("##teamcity[blockOpened name='<CancelReviewInProgressIfAny>']");
+    //     System.out.println("##teamcity[blockOpened name='<CancelReviewInProgressIfAny>']");
 
-        System.out.println("##teamcity[blockClosed name='<CancelReviewInProgressIfAny>']");
+    //     System.out.println("##teamcity[blockClosed name='<CancelReviewInProgressIfAny>']");
 
-        System.out.println("##teamcity[blockOpened name='<CreateConnection>']");
+    //     System.out.println("##teamcity[blockOpened name='<CreateConnection>']");
 
-        System.out.println("Logged in as: Name: 'test'");
-        System.out.println("##teamcity[blockClosed name='<CreateConnection>']");
+    //     System.out.println("Logged in as: Name: 'test'");
+    //     System.out.println("##teamcity[blockClosed name='<CreateConnection>']");
 
-        System.out.println("##teamcity[blockClosed name='<UserRolesTests.CreateAndDeleteUser>']");
+    //     System.out.println("##teamcity[blockClosed name='<UserRolesTests.CreateAndDeleteUser>']");
 
-        System.out.println("##teamcity[blockOpened name='<UserRolesTests.CreateAndDeleteUserRole>']");
+    //     System.out.println("##teamcity[blockOpened name='<UserRolesTests.CreateAndDeleteUserRole>']");
 
-        System.out.println("##teamcity[blockOpened name='<Login>']");
+    //     System.out.println("##teamcity[blockOpened name='<Login>']");
 
-        System.out.println("Login with User: test");
-        System.out.println("Logged in as: Name: 'administrator'");
-        System.out.println("##teamcity[blockClosed name='<Login>']");
+    //     System.out.println("Login with User: test");
+    //     System.out.println("Logged in as: Name: 'administrator'");
+    //     System.out.println("##teamcity[blockClosed name='<Login>']");
 
-        System.out.println("##teamcity[blockOpened name='<CreateNewUserRole>']");
+    //     System.out.println("##teamcity[blockOpened name='<CreateNewUserRole>']");
 
-        System.out.println("##teamcity[blockClosed name='<CreateNewUserRole>']");
-
-
-        try{
-            Thread.sleep(10000);
-        } catch (InterruptedException ie){
-
-        }
-        System.out.println("##teamcity[blockClosed name='some_block']");
+    //     System.out.println("##teamcity[blockClosed name='<CreateNewUserRole>']");
 
 
-        System.out.println("abcdefghijk");
-        System.err.println("abcdefghijk");
+    //     try{
+    //         Thread.sleep(10000);
+    //     } catch (InterruptedException ie){
 
-        System.out.println("http://user:abcdefghijk@jetbrains.com");
-        System.err.println("http://user:abcdefghijk@jetbrains.com");
-
-        System.out.println("##teamcity[blockOpened name='some_block_2']");
-
-
-        assertEquals(2, 2);
-
-        assertEquals("Name not saved correctly", "David", "David");
-        assertEquals("Age not saved correctly 123456", 22, 22);
-        System.out.println("##teamcity[blockClosed name='some_block_3']");
-    }
+    //     }
+    //     System.out.println("##teamcity[blockClosed name='some_block']");
 
 
-    @Test
-    public void TestBlock2() {
-        try{
-            Thread.sleep(10000);
-        } catch (InterruptedException ie){
+    //     System.out.println("abcdefghijk");
+    //     System.err.println("abcdefghijk");
 
-        }
-        System.out.println("##teamcity[blockClosed name='some_block']");
+    //     System.out.println("http://user:abcdefghijk@jetbrains.com");
+    //     System.err.println("http://user:abcdefghijk@jetbrains.com");
 
-
-        System.out.println("abcdefghijk");
-        System.err.println("abcdefghijk");
-
-        System.out.println("http://user:abcdefghijk@jetbrains.com");
-        System.err.println("http://user:abcdefghijk@jetbrains.com");
-
-        System.out.println("##teamcity[blockOpened name='some_block_2']");
+    //     System.out.println("##teamcity[blockOpened name='some_block_2']");
 
 
-        assertEquals(2, 2);
+    //     assertEquals(2, 2);
 
-        assertEquals("Name not saved correctly", "David", "David");
-        assertEquals("Age not saved correctly 123456", 22, 22);
-        System.out.println("##teamcity[blockClosed name='some_block_3']");
-    }
-
-
-    @Test
-    public void TestBlock4() {
-        try{
-            Thread.sleep(10000);
-        } catch (InterruptedException ie){
-
-        }
-        System.out.println("##teamcity[blockClosed name='some_block']");
+    //     assertEquals("Name not saved correctly", "David", "David");
+    //     assertEquals("Age not saved correctly 123456", 22, 22);
+    //     System.out.println("##teamcity[blockClosed name='some_block_3']");
+    // }
 
 
-        System.out.println("abcdefghijk");
-        System.err.println("abcdefghijk");
+    // @Test
+    // public void TestBlock2() {
+    //     try{
+    //         Thread.sleep(10000);
+    //     } catch (InterruptedException ie){
 
-        System.out.println("http://user:abcdefghijk@jetbrains.com");
-        System.err.println("http://user:abcdefghijk@jetbrains.com");
-
-        System.out.println("##teamcity[blockOpened name='some_block_2']");
-
-
-        assertEquals(2, 2);
-
-        assertEquals("Name not saved correctly", "David", "David");
-        assertEquals("Age not saved correctly 123456", 22, 22);
-        System.out.println("##teamcity[blockClosed name='some_block_3']");
-    }
+    //     }
+    //     System.out.println("##teamcity[blockClosed name='some_block']");
 
 
-    @Test
-    public void TestBlock5() {
-        try{
-            Thread.sleep(10000);
-        } catch (InterruptedException ie){
+    //     System.out.println("abcdefghijk");
+    //     System.err.println("abcdefghijk");
 
-        }
-        System.out.println("##teamcity[blockClosed name='some_block']");
+    //     System.out.println("http://user:abcdefghijk@jetbrains.com");
+    //     System.err.println("http://user:abcdefghijk@jetbrains.com");
 
-
-        System.out.println("abcdefghijk");
-        System.err.println("abcdefghijk");
-
-        System.out.println("http://user:abcdefghijk@jetbrains.com");
-        System.err.println("http://user:abcdefghijk@jetbrains.com");
-
-        System.out.println("##teamcity[blockOpened name='some_block_2']");
+    //     System.out.println("##teamcity[blockOpened name='some_block_2']");
 
 
-        assertEquals(2, 2);
+    //     assertEquals(2, 2);
 
-        assertEquals("Name not saved correctly", "David", "David");
-        assertEquals("Age not saved correctly 123456", 22, 22);
-        System.out.println("##teamcity[blockClosed name='some_block_3']");
-    }
+    //     assertEquals("Name not saved correctly", "David", "David");
+    //     assertEquals("Age not saved correctly 123456", 22, 22);
+    //     System.out.println("##teamcity[blockClosed name='some_block_3']");
+    // }
 
-    @Test
-    @Ignore("long name ü")
-    public void TESTUÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜ() {
-        System.out.println("##teamcity[blockOpened name='some_block']");
 
-        assertEquals("David", "David");
-        assertEquals(28, 29);
-    }
+    // @Test
+    // public void TestBlock4() {
+    //     try{
+    //         Thread.sleep(10000);
+    //     } catch (InterruptedException ie){
 
-    @Test
-    @Ignore("long name Ä")
-    public void TESTAÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ() {
-        System.out.println("##teamcity[blockClosed name='some_block']");
+    //     }
+    //     System.out.println("##teamcity[blockClosed name='some_block']");
 
-        assertEquals("Deleting person failed.", 0, 1);
-    }
 
-    @Test
-    public void TESTßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßß(){
-        assertFalse(true);
-    }
+    //     System.out.println("abcdefghijk");
+    //     System.err.println("abcdefghijk");
 
-    @Test
-    @Ignore("ignore test with short name")
-    public void shortTestNameÄÜ(){
-        System.out.println("##teamcity[blockOpened name='some_block']");
+    //     System.out.println("http://user:abcdefghijk@jetbrains.com");
+    //     System.err.println("http://user:abcdefghijk@jetbrains.com");
+
+    //     System.out.println("##teamcity[blockOpened name='some_block_2']");
+
+
+    //     assertEquals(2, 2);
+
+    //     assertEquals("Name not saved correctly", "David", "David");
+    //     assertEquals("Age not saved correctly 123456", 22, 22);
+    //     System.out.println("##teamcity[blockClosed name='some_block_3']");
+    // }
+
+
+    // @Test
+    // public void TestBlock5() {
+    //     try{
+    //         Thread.sleep(10000);
+    //     } catch (InterruptedException ie){
+
+    //     }
+    //     System.out.println("##teamcity[blockClosed name='some_block']");
+
+
+    //     System.out.println("abcdefghijk");
+    //     System.err.println("abcdefghijk");
+
+    //     System.out.println("http://user:abcdefghijk@jetbrains.com");
+    //     System.err.println("http://user:abcdefghijk@jetbrains.com");
+
+    //     System.out.println("##teamcity[blockOpened name='some_block_2']");
+
+
+    //     assertEquals(2, 2);
+
+    //     assertEquals("Name not saved correctly", "David", "David");
+    //     assertEquals("Age not saved correctly 123456", 22, 22);
+    //     System.out.println("##teamcity[blockClosed name='some_block_3']");
+    // }
+
+    // @Test
+    // @Ignore("long name ü")
+    // public void TESTUÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜ() {
+    //     System.out.println("##teamcity[blockOpened name='some_block']");
+
+    //     assertEquals("David", "David");
+    //     assertEquals(28, 29);
+    // }
+
+    // @Test
+    // @Ignore("long name Ä")
+    // public void TESTAÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ() {
+    //     System.out.println("##teamcity[blockClosed name='some_block']");
+
+    //     assertEquals("Deleting person failed.", 0, 1);
+    // }
+
+    // @Test
+    // public void TESTßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßß(){
+    //     assertFalse(true);
+    // }
+
+    // @Test
+    // @Ignore("ignore test with short name")
+    // public void shortTestNameÄÜ(){
+    //     System.out.println("##teamcity[blockOpened name='some_block']");
         
-        assertFalse (true);
-    }
+    //     assertFalse (true);
+    // }
 
-    @Test
-    @Ignore("ignore test with simple name")
-    public void shuldBeIgnored(){
-        assertTrue(false);
-    }
+    // @Test
+    // @Ignore("ignore test with simple name")
+    // public void shuldBeIgnored(){
+    //     assertTrue(false);
+    // }
 }
