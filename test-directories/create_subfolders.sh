eccaa8c8d8e67b8c8f4495a21af41dd4630f031b
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2019-03-26 17:28:11 +0100

    add many directories

---
+++ test-directories/create_subfolders.sh
@@ -0,0 +1,16 @@
+for i in {1..100};
+do
+DIRECTORY_NAME=directory$i
+echo "CREATING DIRECTORY $DIRECTORY_NAME"
+mkdir $DIRECTORY_NAME
+touch file$i.txt
+cd $DIRECTORY_NAME
+for j in {1..100};
+do
+SUBDIRECTORY_NAME=subdirectory$j
+echo "CREATING SUBDIRECTORY $SUBDIRECTORY_NAME"
+mkdir $SUBDIRECTORY_NAME
+touch file$j.txt
+done
+cd ..
+done
